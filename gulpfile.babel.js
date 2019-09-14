04ecb61b6dbb7f172e7aa59fb95b6b3047cebafa
Aaron G <aarongoertzen@gmail.com>
2019-09-14 13:46:00 -0400

    Do not transpile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,12 @@
 import gulp from "gulp";
 import del from "del";
-import babel from "gulp-babel";
 
 const clean = () => del("build");
 
-const bld = () =>
-  gulp
-    .src("src/*.js")
-    .pipe(babel())
-    .pipe(gulp.dest("build/"));
+const copySrc = () => gulp.src("./src/*.js").pipe(gulp.dest("build/"));
 
-const copy = () =>
+const copyRoot = () =>
   gulp.src("./{package.json,LICENSE,README.md}").pipe(gulp.dest("build/"));
 
 // Gulp build task
-export const build = gulp.series(clean, bld, copy); // eslint-disable-line import/prefer-default-export
+export const build = gulp.series(clean, copyRoot, copySrc); // eslint-disable-line import/prefer-default-export
