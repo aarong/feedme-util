23f5be1fbdbfec48551548be78ab2638e7957970
Aaron G <aarongoertzen@gmail.com>
2020-02-19 21:12:24 -0500

    Fix eslint/Prettier problem on index in Node 6

--- src/index.js
+++ src/index.js
@@ -7,25 +7,15 @@ export { default as feedSerializer } from "./feedserializer";
 export { default as md5Calculator } from "./md5calculator";
 export { default as validateAction } from "./validateaction";
 export { default as validateActionResponse } from "./validateactionresponse";
-export {
-  default as validateActionRevelation
-} from "./validateactionrevelation";
+export { default as validateActionRevelation } from "./validateactionrevelation";
 export { default as validateClientMessage } from "./validateclientmessage";
 export { default as validateDelta } from "./validatedelta";
 export { default as validateFeedClose } from "./validatefeedclose";
-export {
-  default as validateFeedCloseResponse
-} from "./validatefeedcloseresponse";
+export { default as validateFeedCloseResponse } from "./validatefeedcloseresponse";
 export { default as validateFeedOpen } from "./validatefeedopen";
-export {
-  default as validateFeedOpenResponse
-} from "./validatefeedopenresponse";
+export { default as validateFeedOpenResponse } from "./validatefeedopenresponse";
 export { default as validateFeedTermination } from "./validatefeedtermination";
 export { default as validateHandshake } from "./validatehandshake";
-export {
-  default as validateHandshakeResponse
-} from "./validatehandshakeresponse";
+export { default as validateHandshakeResponse } from "./validatehandshakeresponse";
 export { default as validateServerMessage } from "./validateservermessage";
-export {
-  default as validateViolationResponse
-} from "./validateviolationresponse";
+export { default as validateViolationResponse } from "./validateviolationresponse";
